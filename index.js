db2a8dc2c63b64e8d18c620464ad27a5d6702b27
silvercanopus <76857954+silvercanopus@users.noreply.github.com>
2024-07-12 18:52:34 +0700

    initial commit

---
+++ index.js
@@ -0,0 +1,35 @@
+// Common imports
+const path = require('path');
+
+// Initialize Express
+const express = require('express');
+const app = express();
+
+// Set EJS (with ejsMate) to be the template engine
+const ejsMate = require('ejs-mate');
+app.engine('ejs', ejsMate);
+app.set('view engine', 'ejs');
+
+// Specify the location of the templates
+app.set('views', path.join(__dirname, 'views'));
+
+// Start the server on port 3000 (no env variables yet)
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Listening on port ${PORT}`);
+})
+
+// Create route for the home page
+app.get('/', (req, res) => {
+    res.render('index');
+})
+
+// Create route for the blog page
+app.get('/blog', (req, res) => {
+    res.render('blog');
+})
+
+// Create route for the contact page
+app.get('/contact', (req, res) => {
+    res.render('contact');
+})
