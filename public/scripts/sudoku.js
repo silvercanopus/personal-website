f42a4849315a31957aebc9190989315278db3102
silvercanopus <76857954+silvercanopus@users.noreply.github.com>
2024-07-19 10:35:43 +0700

    sudoku page - add board state validation

--- public/scripts/sudoku.js
+++ public/scripts/sudoku.js
@@ -68,6 +68,94 @@ class Sudoku {
         }
         this.changeSquareAt(...this.#focus, num);
     }
+
+    // Validate the current board state, returning an object:
+    // - gameover: boolean value indicating whether the game is over or not
+    // - empty: array of coordinates whose square is empty
+    // - conflict: array of coordinates whose square conflict with another square
+    validate () {
+        const res = {
+            gameover: false,
+            empty: [],
+            conflict: []
+        };
+
+        // Scan for empty squares
+        for (let r = 0; r < 9; r++) {
+            for (let c = 0; c < 9; c++) {
+                if (this.#board[r][c] == 0) {
+                    res.empty.push([r, c]);
+                }
+            }
+        }
+
+        // Array and function to help with checking conflicts
+        const conflictBoard = new Array(9).fill(0).map(() => new Array(9).fill(false));
+        const checkGroup = function(group) {
+            // group consists of arrays of [num, row, col]
+            const exists = new Array(10).fill(null);
+            for (let [num, r, c] of group) {
+                if (exists[num]) {
+                    conflictBoard[r][c] = true;
+                    conflictBoard[exists[num][0]][exists[num][1]] = true;
+                }
+                else {
+                    exists[num] = [r, c];
+                }
+            }
+        }
+
+        // Scan for conflicting squares - rows
+        for (let r = 0; r < 9; r++) {
+            const group = [];
+            for (let c = 0; c < 9; c++) {
+                if (this.#board[r][c] != 0) {
+                    group.push([this.#board[r][c], r, c]);
+                }
+            }
+            checkGroup(group);
+        }
+
+        // Scan for conflicting squares - columns
+        for (let c = 0; c < 9; c++) {
+            const group = [];
+            for (let r = 0; r < 9; r++) {
+                if (this.#board[r][c] != 0) {
+                    group.push([this.#board[r][c], r, c]);
+                }
+            }
+            checkGroup(group);
+        }
+
+        // Scan for conflicting squares - 3x3 sections
+        for (let i = 0; i < 9; i += 3) {
+            for (let j = 0; j < 9; j += 3) {
+                const group = [];
+                for (let r = i; r < i+3; r++) {
+                    for (let c = j; c < j+3; c++) {
+                        if (this.#board[r][c] != 0) {
+                            group.push([this.#board[r][c], r, c]);
+                        }
+                    }
+                }
+                checkGroup(group);
+            }
+        }
+
+        // Add any conflicting squares to the result array
+        for (let r = 0; r < 9; r++) {
+            for (let c = 0; c < 9; c++) {
+                if (conflictBoard[r][c]) {
+                    res.conflict.push([r, c]);
+                }
+            }
+        }
+
+        // Game is considered over if there's no empty square and no conflict
+        res.gameover = (res.empty.length == 0) && (res.conflict.length == 0);
+
+        return res;
+    }
 }
 
 /* ===== CONTROLLER ====== */
@@ -87,6 +175,9 @@ const numberButtons = [...document.querySelectorAll('.sudoku-number-button')];
 // Function for rendering the current board state onto the page
 function render() {
     const board = sudoku.getBoard();
+    const val = sudoku.validate();
+
+    // Render the board
     for (let r = 0; r < 9; r++) {
         for (let c = 0; c < 9; c++) {
             if (1 <= board[r][c] && board[r][c] <= 9) {
@@ -96,13 +187,20 @@ function render() {
                 squares[r][c].innerText = "";
             }
             squares[r][c].classList.remove('focused');
+            squares[r][c].classList.remove('conflict');
         }
     }
 
+    // Add focus indicator
     const focus = sudoku.getFocus();
     if (focus) {
         let [r, c] = focus;
         squares[r][c].classList.add('focused');
+    }
+
+    // Add conflict indicator
+    for (const [r, c] of val.conflict) {
+        squares[r][c].classList.add('conflict');
     }
 }
 
